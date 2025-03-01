855c0e56c9ac1c1284b898654153ad867e2bda9c
Maikol Guzmán Alán <mikeguzman@gmail.com>
2025-03-01 09:49:47 -0600

    Version updated to Android 35 and AGP: 8.8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "edu.mike.frontend.taskapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "edu.mike.frontend.taskapp"
         minSdk = 32
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
