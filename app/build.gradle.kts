c4d0183f2159eaa23b0f2e54062aeb9c71803d7f
Maikol Guzmán Alán <mikeguzman@gmail.com>
2026-06-02 17:30:22 -0600

    Update targetSdk to 37 and streamline Gradle version catalog by removing unused Kotlin and Kapt plugin definitions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "edu.mike.frontend.taskapp"
         minSdk = 32
-        targetSdk = 35
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
